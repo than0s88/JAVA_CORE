f8e12ce27c4aad19c62dff9ce3e00852eeaf984f
than0s88 <108162296+than0s88@users.noreply.github.com>
2022-08-31 20:32:26 +0800

    Answered LeetCode problem 4 from home

--- src/main/java/LeetCode/Easy/Problem_4/RomanToInteger.java
+++ src/main/java/LeetCode/Easy/Problem_4/RomanToInteger.java
@@ -5,82 +5,37 @@ import java.util.HashMap;
 public class RomanToInteger {
     public static void main(String[] args) {
 
-        romanToInt("MCMXCIV");
-        //System.out.println(romanToInt("MCMXCIV")); //1994
+        //romanToInt("MCMXCIV");
+        System.out.println(romanToInt("LVIII")); //1994
     }
 
-//    public static int romanToInt(String s) {
-//
-//        HashMap<Character,Integer> hashMap = new HashMap<>();
-//
-//        hashMap.put('I',1);
-//        hashMap.put('V',5);
-//        hashMap.put('X',10);
-//        hashMap.put('L',50);
-//        hashMap.put('C',100);
-//        hashMap.put('D',500);
-//        hashMap.put('M',1000);
-//
-//        int sum =0;
-//        int previous = 0;
-//        int count = 0;
-//
-//        while (count<s.length()) {
-//            int current = hashMap.get(s.charAt(count));
-//
-//            sum += current;
-//
-//            if (previous < current && previous != 1){
-//                sum -= previous;
-//            }
-//            if (previous < current && previous == 1) {
-//                sum -= 2;
-//            }
-//            count++;
-//            previous = current;
-//        }
-//            return sum;
-//    }
+    public static int romanToInt(String s) {
 
+        HashMap<Character, Integer> hashMap = new HashMap<>();
 
-    public static void romanToInt(String s) {
+        hashMap.put('I', 1);
+        hashMap.put('V', 5);
+        hashMap.put('X', 10);
+        hashMap.put('L', 50);
+        hashMap.put('C', 100);
+        hashMap.put('D', 500);
+        hashMap.put('M', 1000);
 
-        HashMap<Character,Integer> hashMap = new HashMap<>();
-
-        hashMap.put('I',1);
-        hashMap.put('V',5);
-        hashMap.put('X',10);
-        hashMap.put('L',50);
-        hashMap.put('C',100);
-        hashMap.put('D',500);
-        hashMap.put('M',1000);
-
-        int sum =0;
+        int sum = 0;
         int previous = 0;
         int count = 0;
 
-        // romanToInt("MCMXCIV"); 1994
-
-        while (count<s.length()) {
+        while (count < s.length()) {
             int current = hashMap.get(s.charAt(count));
-
-            if (previous > current){
-                current = current - previous;
-            }else {
-                sum += current;
+            if (previous < current) {
+                current -= previous * 2;
             }
-//
-//
-//            if (previous < current && previous == 1) {
-//                sum -= 2;
-//            }
 
+            sum += current;
             previous = current;
             count++;
-
-            System.out.print(sum+",");
         }
-        System.out.println("\nSum: "+sum);
-
+        return sum;
     }
+
 }
